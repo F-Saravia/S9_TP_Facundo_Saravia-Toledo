193139a9d567216fd69542e0d2069ea966b1a270
Facundo SARAVIA TOLEDO <saraviatoledo.facundo@gmail.com>
2022-04-04 09:52:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# S9_TP_Facundo_Saravia-Toledo
+ Semaine 9, J41-45, creation d'une application web
---
+++ src/main/java/sem9TP/S9TpFacundoSaraviaToledoApplication.java
@@ -0,0 +1,13 @@
+package sem9TP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S9TpFacundoSaraviaToledoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S9TpFacundoSaraviaToledoApplication.class, args);
+	}
+
+}
